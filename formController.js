f0b9fc31b22f1bd07f1fcc1f375659dd1a8a16d5
hdg-utd <hdg150130@utdallas.edu>
2017-09-04 13:25:55 -0500

    Added validation for about view

--- formController.js
+++ formController.js
@@ -1,5 +1,32 @@
 citizenshipApp.controller('FormController', function($scope, $location) {
     $scope.nameDone = function() {
+        $location.path('/about');
+    }
+    
+    $scope.monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    
+    $scope.dateIsValid = true;
+    
+    $scope.aboutDone = function() {
+        if (dateValidator($scope.user.birthDay, $scope.user.birthMonth, $scope.user.birthYear)) {
+            $scope.dateIsValid = true;
+        } else {
+            $scope.dateIsValid = false;
+        }
         
+        if($scope.dateIsValid) {
+            $location.path('/contact');
+        }
     }
 });
+
+function dateValidator(day, month, year) {
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var inputDate = moment([year, months.indexOf(month), day]);
+    var todayDate = moment();
+    if(inputDate._isValid) {
+        return true;
+    } else {
+        return false;
+    }
+}
